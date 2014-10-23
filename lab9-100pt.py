f70d5f8caa61449dea28336d15421dbe0944217e
DrakeC <s581300@student.roundrockisd.org>
2014-10-23 14:05:11 -0500

    Welllllp
    Workin' on dis still.

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -21,16 +21,21 @@ print 'Hello, welcome to Silent Hill General Hospital. May I please have your te
 temp = int(raw_input())
 if temp > 100:
     if temp > 102:
-        print 'You can go down to room 103, the doctor will be with you shortly.'
+        if temp > 105:
+            print 'You can go down to room 103, the doctor will be with you shortly.'
+        if temp <= 105:
+            print 'Have you been sick in the last 24 hours?'
+            sickHr = raw_input()
+            if sickHr == 'yes' or sickHr == 'y' or sickHr == 'Yes':
+                print 'Room 78 is down the hall to your left.'
+            else:
+                print 'Come back if you start feeling worse.'
+    
+            
     
 else:
     if temp > 102:
-        print 'Have you been sick in the last 24 hours?'
-        sickHr = raw_input()
-        if sickHr == 'yes' or sickHr == 'y' or sickHr == 'Yes':
-            print 'Room 78 is down the hall to your left.'
-        else:
-            print 'Come back if you start feeling worse.'
+        
     else:
         if temp > 100:
             print 'Have you been sick in the last 24 hours?'
@@ -44,6 +49,8 @@ else:
                 print 'Have you recently been to West Africa?'
                 wA = raw_input()
                 if wA == 'yes' or sickHr == 'y' or sickHr == 'Yes':
-                    print 'Go into the quarantine room, the ebola specialists will join you soon.'
-elif temp
-    print 'Come back if you start feeling worse.'
+                    print 'Go into the quarantine room, the ebola specialists will join you soon.' 
+#elif temp > 105
+    
+    
+#    print 'Come back if you start feeling worse.'
